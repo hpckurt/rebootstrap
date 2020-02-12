2701c6615e40b506aaa7f8c967b5be53c64ba647
Helmut Grohne <helmut@subdivi.de>
2020-02-12 19:50:58 +0100

    LIMITS_H_TEST is still broken in gcc-10

--- bootstrap.sh
+++ bootstrap.sh
@@ -885,6 +885,8 @@ patch_gcc_9() {
 	patch_gcc_wdotap
 }
 patch_gcc_10() {
+	echo "fix LIMITS_H_TEST again https://gcc.gnu.org/bugzilla/show_bug.cgi?id=80677"
+	drop_privs sed -i -e 's,^\(+LIMITS_H_TEST = \).*,\1:,' debian/patches/gcc-multiarch.diff
 	patch_gcc_wdotap
 }
 
