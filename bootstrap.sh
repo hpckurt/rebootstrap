hpckurt/rebootstrap

commit a1891b5dcd3d8997f287d0b7f71acfcb5d9efff9

improve our LIMITS_H_TEST
LIMITS_H_TEST continues to be wrong. The original value essentially
expands to:

    [ -f $(BUILD_SYSTEM_HEADER_DIR)/limits.h ]

For wdotap builds, BUILD_SYSTEM_HEADER_DIR expands to /sys-include. The
test fails and gcc stage3 misbuilds.

In the debian packaging a patch adds:

    -o -f $(BUILD_SYSTEM_HEADER_DIR)/$(MULTIARCH_DIRNAME)/limits.h

This ended up not helping as BUILD_SYSTEM_HEADER_DIR is not what we
expect it to be.

Now rebootstrap changed it to `:`. This value happens to break the
stage1 build as there limits.h actually does not exist. When gcc assumes
existence, cpp fails early and the cpp detection prefers using
`/lib/cpp` which causes lots of issues.

So we now change it to the very unportable
/usr/include/$(MULTIARCH_DIRNAME)/limits.h to have it work at all.
Unfortunately, this is not upstreamable.

bootstrap.sh
@@ -736,7 +736,7 @@ EOF
 }
 patch_gcc_limits_h_test() {
 	echo "fix LIMITS_H_TEST again https://gcc.gnu.org/bugzilla/show_bug.cgi?id=80677"
-	drop_privs sed -i -e 's,^\(+LIMITS_H_TEST = \).*,\1:,' debian/patches/gcc-multiarch.diff
+	drop_privs sed -i -e '/^+LIMITS_H_TEST = /s,-o -f \$\$(BUILD_SYSTEM_HEADER_DIR),-o -f /usr/include,' debian/patches/gcc-multiarch.diff
 }
 patch_gcc_wdotap() {
 	if test "$ENABLE_MULTIARCH_GCC" = yes; then

commit 99b64b49a65b5f18ba3ecd834f1df0de5509e9a2

fix excessive escaping in parent commit
It actually became a noop as the pattern didn't match.

Fixes: a1891b5dcd3d ("improve our LIMITS_H_TEST")

bootstrap.sh
@@ -736,7 +736,7 @@ EOF
 }
 patch_gcc_limits_h_test() {
 	echo "fix LIMITS_H_TEST again https://gcc.gnu.org/bugzilla/show_bug.cgi?id=80677"
-	drop_privs sed -i -e '/^+LIMITS_H_TEST = /s,-o -f \$\$(BUILD_SYSTEM_HEADER_DIR),-o -f /usr/include,' debian/patches/gcc-multiarch.diff
+	drop_privs sed -i -e '/^+LIMITS_H_TEST = /s,-o -f \$(BUILD_SYSTEM_HEADER_DIR),-o -f /usr/include,' debian/patches/gcc-multiarch.diff
 }
 patch_gcc_wdotap() {
 	if test "$ENABLE_MULTIARCH_GCC" = yes; then