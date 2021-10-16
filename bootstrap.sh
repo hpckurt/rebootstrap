b95594ae97083ee4639159045d31c9f3385dddbb
Helmut Grohne <helmut@subdivi.de>
2021-10-16 13:38:53 +0200

    fix systemd FTCBFS #996501

--- bootstrap.sh
+++ bootstrap.sh
@@ -1922,6 +1922,12 @@ add_automatic shadow
 add_automatic slang2
 add_automatic spdylay
 add_automatic sqlite3
+
+patch_systemd() {
+	echo "fix FTCBFS #996501"
+	drop_privs sed -i -e 's/\(python3-jinja2\),/\1:native,/' debian/control
+}
+
 add_automatic sysvinit
 
 add_automatic tar
