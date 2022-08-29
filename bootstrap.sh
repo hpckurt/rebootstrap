749695740097d1e16772f19fd8ee1d5bfa142046
Helmut Grohne <helmut@subdivi.de>
2022-08-29 06:02:03 +0200

    drop support for gcc-11

--- bootstrap.sh
+++ bootstrap.sh
@@ -941,14 +941,6 @@ patch_gcc_wdotap() {
 		drop_privs rm -Rf .pc
 	fi
 }
-patch_gcc_11() {
-	# do build common libraries
-	drop_privs sed -i -e 's/^\s*#\?\(with_common_libs\s*:\?=\).*/\1yes/' debian/rules.defs
-	patch_gcc_limits_h_test
-	patch_gcc_default_pie_everywhere
-	patch_gcc_arc_multilib_multiarch
-	patch_gcc_wdotap
-}
 patch_gcc_12() {
 	patch_gcc_limits_h_test
 	patch_gcc_default_pie_everywhere
