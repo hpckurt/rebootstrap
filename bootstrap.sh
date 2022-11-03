c431e60ee99dff5aa0ecaa62a99a874b45195010
Helmut Grohne <helmut@subdivi.de>
2022-11-03 09:27:53 +0100

    work around libevent symbols FTBFS #1023284

--- bootstrap.sh
+++ bootstrap.sh
@@ -1096,7 +1096,13 @@ add_automatic libbsd
 add_automatic libcap2
 add_automatic libdebian-installer
 add_automatic libev
+
 add_automatic libevent
+buildenv_libevent() {
+	echo "work around ftbfs #1023284"
+	export DPKG_GENSYMBOLS_CHECK_LEVEL=0
+}
+
 add_automatic libffi
 
 add_automatic libgc
