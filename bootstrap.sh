hpckurt/rebootstrap

commit 5dc33e116f4301bc0cc336d18b5115f65dcd00f8

populate wdotap patches for gcc-13 #1028094

bootstrap.sh
@@ -200,6 +200,10 @@ if test "$ENABLE_MULTIARCH_GCC" = yes; then
 	apt_get_install cross-gcc-dev
 	echo "removing unused unstripped_exe patch"
 	sed -i '/made-unstripped_exe-setting-overridable/d' /usr/share/cross-gcc/patches/gcc-*/series
+	if ! test -d "/usr/share/cross-gcc/patches/gcc-$GCC_VER"; then
+		# missing patch stack #1028094
+		cp -R "/usr/share/cross-gcc/patches/gcc-$((GCC_VER - 1))" "/usr/share/cross-gcc/patches/gcc-$GCC_VER"
+	fi
 fi
 
 obtain_source_package() {

commit 95a19723f37f05cc4a0cc6dc485fe5621ce4d5f5

initialize GCC_VER before using it
Fixes: 5dc33e116f43 ("populate wdotap patches for gcc-13 #1028094")
Reported-by: Wookey <wookey@debian.org>

bootstrap.sh
@@ -196,6 +196,10 @@ drop_privs() {
 	( drop_privs_exec "$@" )
 }
 
+if test -z "$GCC_VER"; then
+	GCC_VER=`apt-cache depends gcc | sed 's/^ *Depends: gcc-\([0-9.]*\)$/\1/;t;d'`
+fi
+
 if test "$ENABLE_MULTIARCH_GCC" = yes; then
 	apt_get_install cross-gcc-dev
 	echo "removing unused unstripped_exe patch"
@@ -240,10 +244,6 @@ grep -q '^deb-src .*sid' /etc/apt/sources.list.d/*.list || echo "deb-src $MIRROR
 
 dpkg --add-architecture $HOST_ARCH
 $APT_GET update
-
-if test -z "$GCC_VER"; then
-	GCC_VER=`apt-cache depends gcc | sed 's/^ *Depends: gcc-\([0-9.]*\)$/\1/;t;d'`
-fi
 
 rm -Rf /tmp/buildd
 drop_privs mkdir -p /tmp/buildd