582652f989ffe4949e7986808e5d60c72443ab6d
Helmut Grohne <helmut@subdivi.de>
2023-06-13 18:23:15 +0200

    do build common libraries for gcc-12

--- bootstrap.sh
+++ bootstrap.sh
@@ -904,6 +904,7 @@ patch_gcc_12() {
 	patch_gcc_crypt_h
 	patch_gcc_has_include_next
 	patch_gcc_loong64
+	drop_privs sed -i -e 's/^\s*#\?\(with_common_libs\s*:\?=\).*/\1yes/' debian/rules.defs
 	patch_gcc_wdotap
 }
 patch_gcc_13() {
