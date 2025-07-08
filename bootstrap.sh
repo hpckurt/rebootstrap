hpckurt/rebootstrap

commit fda6e7c86449f85db8d13723ec050dd91eba0e2f

Drop hurd-amd64-specific rules to move headers to multiarch directory
Like for hurd-i386 above, this is already covered by the generic rules.

bootstrap.sh
@@ -2509,6 +2509,14 @@ EOF
  endef
  
  # FIXME: We are having runtime issues with ifunc...
+--- a/debian/sysdeps/hurd-amd64.mk
++++ b/debian/sysdeps/hurd-amd64.mk
+@@ -18,5 +18,2 @@ endif
+ define libc_extra_install
+-mkdir -p $(debian-tmp)/usr/include/$(DEB_HOST_MULTIARCH)/mach
+-mv $(debian-tmp)/usr/include/mach/x86_64 $(debian-tmp)/usr/include/$(DEB_HOST_MULTIARCH)/mach/
+-ln -s ../$(DEB_HOST_MULTIARCH)/mach/x86_64 $(debian-tmp)/usr/include/mach/x86_64       
+ endef
 EOF
 }
 buildenv_glibc() {

commit facd8c9020aa756ac12bc933a5e11d29ee67d246

fix glibc hurd-amd64 patch
Fixes: fda6e7c86449 ("Drop hurd-amd64-specific rules to move headers to multiarch directory")

bootstrap.sh
@@ -2515,7 +2515,7 @@ EOF
  define libc_extra_install
 -mkdir -p $(debian-tmp)/usr/include/$(DEB_HOST_MULTIARCH)/mach
 -mv $(debian-tmp)/usr/include/mach/x86_64 $(debian-tmp)/usr/include/$(DEB_HOST_MULTIARCH)/mach/
--ln -s ../$(DEB_HOST_MULTIARCH)/mach/x86_64 $(debian-tmp)/usr/include/mach/x86_64       
+-ln -s ../$(DEB_HOST_MULTIARCH)/mach/x86_64 $(debian-tmp)/usr/include/mach/x86_64
  endef
 EOF
 }
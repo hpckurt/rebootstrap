facd8c9020aa756ac12bc933a5e11d29ee67d246
Helmut Grohne <helmut@subdivi.de>
2025-07-08 20:12:23 +0200

    fix glibc hurd-amd64 patch
    Fixes: fda6e7c86449 ("Drop hurd-amd64-specific rules to move headers to multiarch directory")

--- bootstrap.sh
+++ bootstrap.sh
@@ -2515,7 +2515,7 @@ EOF
  define libc_extra_install
 -mkdir -p $(debian-tmp)/usr/include/$(DEB_HOST_MULTIARCH)/mach
 -mv $(debian-tmp)/usr/include/mach/x86_64 $(debian-tmp)/usr/include/$(DEB_HOST_MULTIARCH)/mach/
--ln -s ../$(DEB_HOST_MULTIARCH)/mach/x86_64 $(debian-tmp)/usr/include/mach/x86_64       
+-ln -s ../$(DEB_HOST_MULTIARCH)/mach/x86_64 $(debian-tmp)/usr/include/mach/x86_64
  endef
 EOF
 }
