9405199dc670be1888fb0b9d2e3e1c37863db2ff
Helmut Grohne <helmut@subdivi.de>
2025-11-26 09:19:50 +0100

    Rename DIST to SUITE
    The variable name "DIST" is fairly generic and might end up becoming
    ambiguous. Choose "SUITE" instead as that's what apt uses.

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,7 +19,7 @@ DEFAULT_PROFILES="cross nocheck noinsttest noudeb"
 DROP_PRIVS=buildd
 GCC_NOLANG="ada algol asan brig cobol d gcn go itm java jit hppa64 lsan m2 nvptx objc obj-c++ rust tsan ubsan"
 ENABLE_DIFFOSCOPE=no
-DIST=sid
+SUITE=sid
 
 if df -t tmpfs /var/cache/apt/archives >/dev/null 2>&1; then
 	APT_GET="$APT_GET -o APT::Keep-Downloaded-Packages=false"
@@ -231,7 +231,7 @@ fi
 
 # ensure that the rebootstrap list comes first
 test -f /etc/apt/sources.list && mv -v /etc/apt/sources.list /etc/apt/sources.list.d/local.list
-grep -q '^deb-src .*'"$DIST" /etc/apt/sources.list.d/*.list || echo "deb-src $MIRROR $DIST main" >> /etc/apt/sources.list.d/$DIST-source.list
+grep -q "^deb-src .*$SUITE" /etc/apt/sources.list.d/*.list || echo "deb-src $MIRROR $SUITE main" >> "/etc/apt/sources.list.d/$SUITE-source.list"
 
 dpkg --add-architecture "$HOST_ARCH"
 $APT_GET update
@@ -384,7 +384,7 @@ chdist_native() {
 
 if test "$ENABLE_DIFFOSCOPE" = yes; then
 	apt_get_install devscripts
-	chdist_native create "$MIRROR" "$DIST" main
+	chdist_native create "$MIRROR" "$SUITE" main
 	if ! chdist_native apt-get update; then
 		echo "rebootstrap-warning: not comparing packages to native builds"
 		rm -Rf /tmp/chdist_native
@@ -530,7 +530,7 @@ cross_build_setup() {
 # $2 is reason
 add_binNMU_changelog() {
 	cat - debian/changelog <<EOF |
-$(dpkg-parsechangelog -SSource) ($(dpkg-parsechangelog -SVersion)+b$1) $DIST; urgency=medium, binary-only=yes
+$(dpkg-parsechangelog -SSource) ($(dpkg-parsechangelog -SVersion)+b$1) $SUITE; urgency=medium, binary-only=yes
 
   * Binary-only non-maintainer upload for $HOST_ARCH; no source changes.
   * $2
@@ -2875,7 +2875,7 @@ patch_linux() {
 	local kernel_arch
 	kernel_arch=
 	cat - debian/changelog <<EOF |
-linux ($(dpkg-parsechangelog -SVersion)+rebootstrap1) $DIST; urgency=medium
+linux ($(dpkg-parsechangelog -SVersion)+rebootstrap1) $SUITE; urgency=medium
 
   * Update for $HOST_ARCH
 
