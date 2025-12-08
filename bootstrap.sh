fa0ad37860340b0103a0b23be1bc5efbf0a5f0fb
Helmut Grohne <helmut@subdivi.de>
2025-12-08 13:24:08 +0100

    enable LIMITS_H_TEST patch for gcc-15
    Otherwise the gcc stage3 build will miss a definition of PATH_MAX while
    building libasan for some architectures such as ppc64el.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2587,6 +2587,7 @@ buildenv_gcc_14() {
 	export DPKG_GENSYMBOLS_CHECK_LEVEL=0
 }
 patch_gcc_15() {
+	patch_gcc_limits_h_test
 	patch_gcc_for_host_in_rtlibs
 	patch_gcc_default_pie_everywhere
 	patch_gcc_wdotap
